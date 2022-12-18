96239479f09f24d5f5b33c365232641e1486a44b
OlegPolishchuk <oleg.makkorm@gmail.com>
2022-12-18 19:40:50 +0300

    fix: get review pagination

--- src/controllers/review/getReviews.ts
+++ src/controllers/review/getReviews.ts
@@ -18,17 +18,33 @@ export const getReviews = async (req: Request, res: Response) => {
     const sort = sortName ? {[sortName]: -1} : {created: -1};
     const searchId = id ? {_id: id} : {};
 
+    // const reviews = await Reviews
+    //   .find(searchId)
+    //   .sort(sort as { [p: string]: SortOrder })
+    //   .skip(skipCount)
+    //   .limit(limitNumber);
+
     const reviews = await Reviews
       .find(searchId)
       .sort(sort as { [p: string]: SortOrder })
-      .skip(skipCount)
-      .limit(limitNumber);
+
+    const resultReviews = reviews.slice(skipCount, limitNumber + skipCount);
+
+    // const reviews = await Reviews
+    //   .find(searchId)
+    //   .sort({overallScore: -1})
+    //   .skip(0)
+    //   .limit(10);
+
 
     const totalCount = await Reviews.find().count();
 
     res.status(200).json({
       totalCount,
-      reviews
+      reviews: resultReviews,
+      skip: skipCount,
+      limit: limitNumber,
+      sorted: await Reviews.find(searchId).sort(sort as {[p: string]: SortOrder })
     })
   }
   catch (e) {
